c784e5855091a087cf09e7aca7d8b1b841dfedf8
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-28 17:39:26 +0100

    :art: Nav   :art:

---
+++ app/components/Navigation/ResponsiveNav.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import React from 'react'
+import Nav from "./Nav"
+import MobileNav from './MobileNav'
+
+
+const ResponsiveNav = () => {
+  return (
+    <div>
+        <Nav/>
+        <MobileNav/>
+    </div>
+  )
+}
+
+export default ResponsiveNav
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Home from "./components/Home"
 
 const HomePage = () => {
   return (
-    <div className='text-9xl text-red-900'>HomePage</div>
+    <div >
+        <Home/>
+    </div>
   )
 }
 
