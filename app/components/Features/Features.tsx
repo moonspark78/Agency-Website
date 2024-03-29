eac28898a8410b132751190d9f61e9094f102751
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-29 17:55:51 +0100

    :art: Starting with the Features section   :art:

---
+++ app/components/Features/Features.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SectionHeading from '../SectionHeading/SectionHeading'
+
+const Features = () => {
+  return (
+
+    <div className='pt-[5rem] pb-[3rem]'>
+        <SectionHeading headingMini= "Ideal Solution for you" headingPrimary="Explore Utimate features"/>
+    </div>
+  )
+}
+
+export default Features
--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <div className="h-[70vh] lg:h-[88vh] md:h-[80vh] sm:h-[75vh] flex items-center flex-col justify-center">
       <div className="grid grid-cols-1 lg:grid-cols-5 items-center w-[80%] mx-auto">
         <div className="col-span-2">
-          <h1 className="text-[27px] md:text-[35px] lg:text-[40px] mb-[1rem] font-bold text-[#02073e] leading-[2.4rem] md:leading-[ 4rem]">
+          <h1 className="text-[27px] md:text-[35px] lg:text-[40px] mb-[1rem] font-bold text-[#02073e] leading-[2.4rem] md:leading-[4rem]">
             Exploring Innovative Paths to Cultivate Your Business
           </h1>
           <p className="md:text-[17px] text-[15px] mb-[2rem] text-black opacity-90 font-[400]">
