3479403060520361492e6b6eb50a8f40b206d843
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-30 14:27:46 +0100

    :art: Done  with the Customer Section    :art:

--- app/components/CustomerSupport/CustomerSupport.tsx
+++ app/components/CustomerSupport/CustomerSupport.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import React from "react";
 import SupportImg from "@/public/images/support.jpg";
+import { CheckIcon } from "@heroicons/react/16/solid";
 
 const CustomerSupport = () => {
   return (
-    <div className="pt-[5rem] pb-[3rem]">
+    <div className="pt-[4rem] pb-[3rem]">
       <div className="w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-[4rem] items-center">
         <div className="order-2 lg:order-1">
           <Image src={SupportImg} alt="support" />
@@ -14,8 +15,27 @@ const CustomerSupport = () => {
             Customer support is our main priority with their hundred percent
             satisfaction.
           </h1>
-          <p className="mt-[1.5rem] mb-[1.5rem] text-black opacity-90 text-[15px] md:text-[16px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-             Quidem eligendi dolore esse autem aliquam odio sint aspernatur, amet alias quibusdam.</p>
+          <p className="mt-[1.5rem] mb-[1.5rem] text-black opacity-90 text-[15px] md:text-[16px]">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem
+            eligendi dolore esse autem aliquam odio sint aspernatur, amet alias
+            quibusdam.
+          </p>
+          <div className="flex items-center mb-[1rem] space-x-3">
+            <CheckIcon className="w-[1.3rem] h-[1.3rem] text-red-600"/>
+            <p className="text-[17px] text-[#02073e] font-[500]">Medical and Vision</p>
+          </div>
+          <div className="flex items-center mb-[1rem] space-x-3">
+            <CheckIcon className="w-[1.3rem] h-[1.3rem] text-red-600"/>
+            <p className="text-[17px] text-[#02073e] font-[500]">400(k) Savings </p>
+          </div>
+          <div className="flex items-center mb-[1rem] space-x-3">
+            <CheckIcon className="w-[1.3rem] h-[1.3rem] text-red-600"/>
+            <p className="text-[17px] text-[#02073e] font-[500]">Life Insurance</p>
+          </div>
+          <div className="flex items-center mb-[1rem] space-x-3">
+            <CheckIcon className="w-[1.3rem] h-[1.3rem] text-red-600"/>
+            <p className="text-[17px] text-[#02073e] font-[500]">HSAs and FSAs</p>
+          </div>
         </div>
       </div>
     </div>
